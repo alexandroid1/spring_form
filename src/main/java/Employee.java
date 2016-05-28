b233d312071e037220db9faaf9f171e2f8cdc43e
Oleksandr <avpavlov96@gmail.com>
2016-05-28 23:26:53 +0300

    class Employee

---
+++ src/main/java/Employee.java
@@ -0,0 +1,10 @@
+/**
+ * Created by ALEX on 28.05.2016.
+ */
+public class Employee {
+    private String name;
+    private long id;
+    private String contactNumber;
+
+    // standard getters and setters
+}
